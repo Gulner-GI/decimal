4ffb53cdf0cce878fb6735b26ad6e9f3fd91d54f
Gulner-GI <yura.kuzin1990@gmail.com>
2026-01-21 16:06:29 +0300

    develop converse_to_decimal

--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -4,6 +4,9 @@ typedef struct {
   unsigned int bits[4];
 } s21_decimal;
 
+#define OK 0
+#define ERROR 1
+
 int get_sign(s21_decimal);
 void set_sign(s21_decimal *, int);
 int get_scale(s21_decimal);
@@ -13,3 +16,8 @@ int is_valid_decimal(s21_decimal);
 void reduce_scale(s21_decimal *);
 int increase_scale(s21_decimal *);
 void normalize_scale(s21_decimal *, s21_decimal *);
+
+int s21_from_int_to_decimal(int src, s21_decimal *dst);
+int s21_from_float_to_decimal(float src, s21_decimal *dst);
+int s21_from_decimal_to_int(s21_decimal src, int *dst);
+int s21_from_decimal_to_float(s21_decimal src, float *dst);
---
+++ src/s21_decimal_converse.c
@@ -0,0 +1,47 @@
+#include "s21_decimal.h"
+
+int s21_from_int_to_decimal(int src, s21_decimal *dst) {
+  int status = OK;
+  if (!dst) {
+    status = ERROR;
+  } else {
+    unsigned long int src_abs;
+    if (src < 0) {
+      set_sign(dst, 1);
+      src_abs = (unsigned long int)(-(long)src);
+    } else {
+      set_sign(dst, 0);
+      src_abs = src;
+    }
+    dst->bits[0] = src_abs;
+    dst->bits[1] = 0;
+    dst->bits[2] = 0;
+    set_scale(dst, 0);
+  }
+  return status;
+}
+
+int s21_from_float_to_decimal(float src, s21_decimal *dst) {
+  int status = OK;
+  int scale = 0;
+  double src_abs = src;
+  if (src < 0) {
+    set_sign(dst, 1);
+    src_abs = -src_abs;
+  } else {
+    set_sign(dst, 0);
+  }
+  while (src_abs - (long)src_abs > 0 && scale <= 28) {
+    src_abs *= 10;
+    scale++;
+  }
+  dst->bits[0] = (unsigned int)src_abs;
+  dst->bits[1] = 0;
+  dst->bits[2] = 0;
+  set_scale(dst, scale);
+  return status;
+}
+
+int s21_from_decimal_to_int(s21_decimal src, int *dst) {}
+
+int s21_from_decimal_to_float(s21_decimal src, float *dst) {}
